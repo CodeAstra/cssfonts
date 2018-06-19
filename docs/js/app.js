ef6fe6e5dc314ac4a045ab7225ffe5ac367766aa
BV Satyaram <bvsatyaram@gmail.com>
2018-06-19 08:39:22 +0530

    Add down logo

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,4 @@
-var icons = ['add', 'close', 'heart', 'search'];
+var icons = ['add', 'close', 'heart', 'search', 'down'];
 
 var iconContainerTemplate = '<article>' +
   '<span class="css-icon css-icon-{iconName}"></span>' +
